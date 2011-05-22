7c9364c41bee24acba25330c0011c95782a6340b
Matthew Jording <mjording@opengotham.com>
2011-05-22 02:08:01 -0400

    fixed seed w assoc

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,47 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110522034239) do
+ActiveRecord::Schema.define(:version => 20110522050451) do
 
   create_table "games", :force => true do |t|
     t.datetime "starts_at"
     t.integer  "home_team_id"
     t.integer  "away_team_id"
-    t.integer  "venue_id"
+    t.string   "venue"
     t.string   "mlb_gameid"
     t.integer  "inning_state"
     t.string   "league"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.boolean  "in_progress"
+  end
+
+  create_table "players", :force => true do |t|
+    t.string   "first"
+    t.string   "last"
+    t.integer  "number"
+    t.string   "position"
+    t.integer  "mlb_id"
+    t.integer  "team_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "players", ["mlb_id"], :name => "index_players_on_mlb_id"
+
+  create_table "team_player_stats", :force => true do |t|
+    t.integer  "player_id"
+    t.integer  "game_id"
+    t.integer  "team_id"
+    t.string   "position"
+    t.string   "rl"
+    t.string   "status"
+    t.float    "avg"
+    t.integer  "hr"
+    t.integer  "rbi"
+    t.integer  "wins"
+    t.integer  "losses"
+    t.float    "era"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -35,9 +66,12 @@ ActiveRecord::Schema.define(:version => 20110522034239) do
     t.string   "mascot"
     t.string   "ballpark"
     t.string   "logo_url"
+    t.string   "mlb_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "teams", ["mlb_id"], :name => "index_teams_on_mlb_id"
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,7 @@ mo.css('a').each do |day|
            away_team.update_attributes(:name => gamedoc.xpath('//game/team[attribute::type = "away"]').first['name'])
            home_team = Team.find_or_create_by_mlb_id(:mlb_id => gamedoc.xpath('//game/team[attribute::type = "home"]').first['id'])
            home_team.update_attributes(:name => gamedoc.xpath('//game/team[attribute::type = "home"]').first['name'])
-           game.update_attributes(:home_team => home_team,:away_team => away_team, :mlb_gameid => link.text.strip.gsub('/',''))
+           game.update_attributes("home_team_id" => home_team.id,"away_team_id" => away_team.id, :mlb_gameid => link.text.strip.gsub('/',''))
 
            
            gamedoc.xpath('//game/team[attribute::type = "away"]/player').each do|playr|
