e4d7463a0bbd72f79e7a250d4ccbf51efb64e1d2
Matthew Jording <mjording@opengotham.com>
2011-05-22 04:01:56 -0400

    user estimate associations

--- app/models/player_game_stat_estimate.rb
+++ app/models/player_game_stat_estimate.rb
@@ -1,2 +1,3 @@
 class PlayerGameStatEstimate < ActiveRecord::Base
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :avatar, :avatar_cache
-  
+  has_many :player_game_stat_estimates
   mount_uploader :avatar, AvatarUploader
   
   def name
