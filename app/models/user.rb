2d48f50e830b026f2409699eb1e21e4332d0460b
Adam Singer <adam@artforge.com>
2011-05-22 00:24:14 -0400

    More styling goodness added user avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,11 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :avatar, :avatar_cache
+  
+  mount_uploader :avatar, AvatarUploader
+  
+  def name
+    return "#{first_name} #{last_name}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110522010345) do
+ActiveRecord::Schema.define(:version => 20110522034239) do
 
   create_table "games", :force => true do |t|
     t.datetime "starts_at"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(:version => 20110522010345) do
     t.string   "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
