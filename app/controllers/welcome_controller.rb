7495344b0c7b2039927e62aa1e79bc32b891f0dc
Adam Singer <adam@artforge.com>
2011-05-22 05:31:55 -0400

    More view tweaks & time zone fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    # @games = Game.upcoming
-    @games = Game.scoped.order("starts_at desc").limit(10)
+    @recent_games = Game.past.limit(10)
+    @upcoming_games = Game.upcoming.limit(10)
+    # @games = Game.scoped.order("starts_at desc").limit(10)
   end
 end
